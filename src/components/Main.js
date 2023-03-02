62b84665815467f77d987f49a182f85c09a8e0b6
Fialka Lesnaya <listik.vs@gmail.com>
2023-03-02 21:22:47 +0200

    Вызов обработчиков

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,22 +1,22 @@
 function Main() {
 
-    function handleEditAvatarClick() {
-        //document.querySelector('#update-avatar').classNameList.add('popup_opened');
+    const handleEditAvatarClick = () => {
+        document.querySelector('#update-avatar').classList.add('popup_opened');
     }
 
-    function handleEditProfileClick() {
-        //document.querySelector('#edit-profile').classNameList.add('popup_opened');
+    const handleEditProfileClick = () => {
+        document.querySelector('#edit-profile').classList.add('popup_opened');
     }
 
-    function handleAddPlaceClick() {
-        //document.querySelector('#add-card').classNameList.add('popup_opened');
+    const handleAddPlaceClick = () => {
+        document.querySelector('#add-card').classList.add('popup_opened');
     }
 
     return (
         <main>
             <section className="profile">
                 <div className="profile__avatar-wrapper">
-                    <div className="profile__avatar-edit" onClick={handleEditAvatarClick()}></div>
+                    <div className="profile__avatar-edit" onClick={handleEditAvatarClick}></div>
                     <img className="profile__avatar" alt="Аватар пользователя" />
                 </div>
 
@@ -24,13 +24,13 @@ function Main() {
                     <div className="profile__name-container">
                         <h1 className="profile__name"></h1>
 
-                        <button aria-label="Редактировать профиль" type="button" className="profile__edit-button" onClick={handleEditProfileClick()}></button>
+                        <button aria-label="Редактировать профиль" type="button" className="profile__edit-button" onClick={handleEditProfileClick}></button>
                     </div>
 
                     <p className="profile__position"></p>
                 </div>
 
-                <button type="button" className="profile__add-button" onClick={handleAddPlaceClick()}></button>
+                <button type="button" className="profile__add-button" onClick={handleAddPlaceClick}></button>
             </section>
 
             <section className="elements">
