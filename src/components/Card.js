aa5cf17da86c2b3a9956e0c0b23fad1d85cf8bbd
Fialka Lesnaya <listik.vs@gmail.com>
2023-03-10 01:09:12 +0200

    Исправление ошибок

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,13 +2,13 @@ function Card(props) {
     const item = props.card;
 
     return (
-        <article className="elements__item" onClick={() => props.onCardClick(item)}>
-            <div className="elements__trash"></div>
-            <img className="elements__image" src={item.link} />
+        <article className="elements__item">
+            <div className="elements__trash" onClick={props.onDeleteCardClick}></div>
+            <img className="elements__image" src={item.link} onClick={() => props.onCardClick(item)} />
             <div className="elements__info">
                 <h2 className="elements__name" title="">{item.name}</h2>
                 <div className="elements__like-container">
-                    <button type="button" className="elements__like-button" onClick={props.onDeleteCardClick}></button>
+                    <button type="button" className="elements__like-button"></button>
                     <span className="elements__like-count">{item.likes.length}</span>
                 </div>
             </div>
